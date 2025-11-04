b5ff086f2dda6c3673962f4fde695ecef8b24d5a
Saurav pawar <sauravpawar278@gmail.com>
2025-11-04 22:28:57 +0530

    Change pip install command to comment

--- apppp.py
+++ apppp.py
@@ -12,7 +12,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-pip install joblib
+!pip install joblib
 import joblib
 
 from sklearn.model_selection import train_test_split
